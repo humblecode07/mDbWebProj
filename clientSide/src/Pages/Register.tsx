f04309f3acc0856b00ac04a9adfda6ac69811d79
mikoCalderon011 <mikocalderon123@gmail.com>
2024-12-20 10:08:38 -0800

    Commit: Login And Register Component

--- clientSide/src/Pages/Register.tsx
+++ clientSide/src/Pages/Register.tsx
@@ -1,9 +1,13 @@
 import React, { useState } from 'react';
 import { registerUser } from '../api/api';
 import { NavLink, useNavigate } from 'react-router-dom';
+import Marquees from './Marquees';
+import KeyIcon from '../assets/Icons/KeyIcon';
+import EmailIcon from '../assets/Icons/EmailIcon';
+import UserIcon from '../assets/Icons/UserIcon';
 
 const Register = () => {
-const navigate = useNavigate();
+  const navigate = useNavigate();
   const [userDetails, setUserDetails] = useState({
     full_name: '',
     email: '',
@@ -31,68 +35,76 @@ const navigate = useNavigate();
   };
 
   return (
-    <main className="text-white flex flex-col gap-4 font-roboto p-4">
-      <h2 className="text-center text-2xl font-semibold mb-6">Register</h2>
-      
-      {error && <p className="text-red-500 text-center">{error}</p>}
-      
+    <main className="min-h-[41.25rem] text-black flex flex-col gap-0 p-0">
+      {/* <Marquees /> */}
       <form onSubmit={handleSubmit} className="flex flex-col gap-4">
-        <div className="flex flex-col">
-          <label htmlFor="full_name" className="text-gray-700">Full Name</label>
-          <input
-            id="full_name"
-            name="full_name"
-            type="text"
-            value={userDetails.full_name}
-            onChange={handleChange}
-            className="text-white p-2 border border-gray-300 rounded-md bg-transparent"
-            required
-            placeholder="Enter your full name"
-          />
+        <div className='w-[66.5625rem] h-[35.875rem] flex gap-[3.4375rem] relative z-[2] mt-[2.5625rem]'>
+          <div className='w-[30.3125rem] flex flex-col font-passionOne font-bold self-end text-white'>
+            <span className='text-[3.125rem]'>Discover movies.</span>
+            <span className='text-[7.5rem]'>Join tskr<span className='text-[#FF8731]'>!</span></span>
+          </div>
+          <div
+            className='w-[32.8125rem] h-[35.875rem] text-white rounded-[2rem] backdrop-blur-md flex flex-col justify-center items-center'
+            style={{ backgroundColor: 'rgba(0, 0, 0, .2)' }}
+          >
+            <div className='w-[26.15625rem] h-[29.875rem] flex flex-col items-center gap-[3.375rem]'>
+              <span className='font-roboto font-bold text-[1.75rem]'>CREATE ACCOUNT</span>
+              <div className='w-full h-[2.5625rem] border-solid border-b-[1px] border-white flex items-center'>
+                <div className='flex items-center gap-[.875rem] pl-[.4375rem]'>
+                  <UserIcon />
+                  <input
+                    id="full_name"
+                    type='text'
+                    name='full_name'
+                    value={userDetails.full_name}
+                    onChange={handleChange}
+                    placeholder="Full Name"
+                    className='w-[22.5rem] bg-transparent outline-none'
+                  />
+                </div>
+              </div>
+              <div className='w-full h-[2.5625rem] border-solid border-b-[1px] border-white flex'>
+                <div className='flex items-center gap-[.875rem] pl-[.4375rem]'>
+                  <EmailIcon />
+                  <input
+                    type='email'
+                    name='email'
+                    placeholder="Email"
+                    value={userDetails.email}
+                    onChange={handleChange}
+                    className='w-[22.5rem] bg-transparent outline-none'
+                  />
+                </div>
+              </div>
+              <div className='w-full h-[2.5625rem] border-solid border-b-[1px] border-white flex items-center'>
+                <div className='flex items-center gap-[.875rem] pl-[.4375rem]'>
+                  <KeyIcon />
+                  <input
+                    id="password"
+                    type='password'
+                    name='password'
+                    value={userDetails.password}
+                    onChange={handleChange}
+                    placeholder="Password"
+                    className='w-[22.5rem] bg-transparent outline-none'
+                  />
+                </div>
+              </div>
+              <button
+                type="submit"
+                className='w-[16.75rem] h-[3.25rem] text-[#FF8731] border-solid border-[2px] border-[#FF8731] rounded-full'
+              >
+                Register
+              </button>
+              <span className='font-roboto'> Already have an account?
+                <NavLink to="/signin" className="text-blue-600 hover:underline pl-1">
+                  Login
+                </NavLink>
+              </span>
+            </div>
+          </div>
         </div>
-        
-        <div className="flex flex-col">
-          <label htmlFor="email" className="text-gray-700">Email</label>
-          <input
-            id="email"
-            name="email"
-            type="email"
-            value={userDetails.email}
-            onChange={handleChange}
-            className="text-white p-2 border border-gray-300 rounded-md bg-transparent"
-            required
-            placeholder="Enter your email"
-          />
-        </div>
-
-        <div className="flex flex-col">
-          <label htmlFor="password" className="text-gray-700">Password</label>
-          <input
-            id="password"
-            name="password"
-            type="password"
-            value={userDetails.password}
-            onChange={handleChange}
-            className="text-white p-2 border border-gray-300 rounded-md bg-transparent"
-            required
-            placeholder="Enter your password"
-          />
-        </div>
-
-        <button
-          type="submit"
-          className="bg-blue-600 text-white py-2 rounded-md mt-4 hover:bg-blue-700"
-        >
-          Register
-        </button>
       </form>
-
-      <div className="mt-4 text-center">
-        <p className="text-sm text-gray-500">
-          Already have an account? 
-          <NavLink to="/signin" className="text-blue-600 hover:underline">Login</NavLink>
-        </p>
-      </div>
     </main>
   );
 };
--- clientSide/src/Pages/SignIn.tsx
+++ clientSide/src/Pages/SignIn.tsx
@@ -4,6 +4,8 @@ import { jwtDecode } from 'jwt-decode';
 import useAuth from '../hooks/useAuth';
 import { NavLink, useNavigate } from 'react-router-dom';
 import Marquees from './Marquees';
+import EmailIcon from '../assets/Icons/EmailIcon';
+import KeyIcon from '../assets/Icons/KeyIcon';
 
 const userRole = Number(import.meta.env.VITE_YT_ROLE_USER);
 const adminRole = Number(import.meta.env.VITE_YT_ROLE_ADMIN);
@@ -57,8 +59,63 @@ const SignIn = () => {
   };
 
   return (
-    <main className=" min-h-[41.25rem] text-white font-roboto p-0">
-      <Marquees />
+    <main className=" min-h-[41.25rem] text-black flex flex-col gap-0 p-0">
+      {/* <Marquees /> */}
+      <form onSubmit={(e) => { e.preventDefault(); handleLogin(); }} className="flex flex-col gap-4">
+        {error && <p className="text-red-500 text-center">{error}</p>}
+        <div className='w-[66.5625rem] h-[35.875rem] flex gap-[3.4375rem] relative z-[2] mt-[2.5625rem]'>
+          <div className='w-[30.3125rem] flex flex-col font-passionOne font-bold self-end text-white'>
+            <span className='text-[3.125rem]'>Back for more?</span>
+            <span className='text-[5rem]'>Sign in to tskr<span className='text-[#FF8731]'>!</span></span>
+          </div>
+          <div
+            className='w-[32.8125rem] h-[35.875rem] text-white rounded-[2rem] backdrop-blur-md flex flex-col justify-center items-center'
+            style={{ backgroundColor: 'rgba(0, 0, 0, .2)' }}
+          >
+            <div className='w-[26.15625rem] h-[25.4375rem] flex flex-col items-center gap-[3.375rem]'>
+              <span className='font-roboto font-bold text-[1.75rem]'>LOGIN</span>
+              <div className='w-full h-[2.5625rem] border-solid border-b-[1px] border-white flex'>
+                <div className='flex items-center gap-[.875rem] pl-[.4375rem]'>
+                  <EmailIcon />
+                  <input
+                    type='email'
+                    name='email'
+                    placeholder="Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    className='w-[22.5rem] bg-transparent outline-none'
+                  />
+                </div>
+              </div>
+              <div className='w-full h-[2.5625rem] border-solid border-b-[1px] border-white flex items-center'>
+                <div className='flex items-center gap-[.875rem] pl-[.4375rem]'>
+                  <KeyIcon />
+                  <input
+                    id="password"
+                    type='password'
+                    name='password'
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    placeholder="Password"
+                    className='w-[22.5rem] bg-transparent outline-none'
+                  />
+                </div>
+              </div>
+              <button
+                type="submit"
+                className='w-[16.75rem] h-[3.25rem] text-[#FF8731] border-solid border-[2px] border-[#FF8731] rounded-full'
+              >
+                Sign In
+              </button>
+              <span className='font-roboto'>Don't have an account?
+                <NavLink to="/signup" className="text-blue-600 hover:underline pl-1">
+                  Sign up
+                </NavLink>
+              </span>
+            </div>
+          </div>
+        </div>
+      </form>
     </main>
   );
 };
